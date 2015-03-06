de7f5111ea2ccfb6572c5ecf958980ef18decfa9
Joe Gordon <joe.gordon0@gmail.com>
2015-03-05 23:26:08 -0800

    boltdb: preliminary findings
    Not surprisingly doing two disk writes is massively slower then
    single write to RAM.
    
    If do 1million writes per db.Update overhead is much better

--- boltdb/main.go
+++ boltdb/main.go
@@ -8,7 +8,7 @@ Testing out boltdb for the following use case:
 
 Issues so far:
 
-* Writing to boltdb involves 2 writes to disk, so perforamnce is terrible if writes aren't batched
+* Writing to boltdb involves 2 writes to disk, so performance is terrible if writes aren't batched
 
 Ideas to try out:
 
@@ -16,6 +16,15 @@ Ideas to try out:
 * Swap in boltdb backend and compare.
 * Try out boltdb transaction coalescer
   https://github.com/boltdb/coalescer
+* Separate test to measure how long it takes to read all the values back.
+
+
+Findings:
+
+* Overhead of db.Update for single key/value write is massive.
+  At 1 million keys per db.Update overhead  still 5x slower
+
+Next try coalescer
 
 */
 
@@ -25,16 +34,18 @@ import (
 	"fmt"
 	"github.com/boltdb/bolt"
 	"log"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var bucket = []byte("MyBucket")
 
-func main() {
+func prepBolt() *bolt.DB {
 	db, err := bolt.Open("my.db", 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 
 	// create bucket
 	err = db.Update(func(tx *bolt.Tx) error {
@@ -47,8 +58,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	return db
+}
 
-	err = db.Update(func(tx *bolt.Tx) error {
+func hellobolt() {
+	db := prepBolt()
+	defer db.Close()
+
+	err := db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(bucket)
 		err := b.Put([]byte("answer"), []byte("42"))
 		return err
@@ -60,11 +77,66 @@ func main() {
 	err = db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket(bucket)
 		v := b.Get([]byte("answer"))
-		fmt.Printf("value: %s", v)
+		fmt.Printf("value: %s\n", v)
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func keyValue(i int) (key, value string) {
+	key = strconv.Itoa(i)
+	value = strings.Repeat(key, 5)
+	return key, value
+}
+
+func writeMapTest(size int) {
+	db := make(map[(string)]string)
+	var key string
+	var value string
+	for i := 0; i < size; i++ {
+		key, value = keyValue(i)
+		//fmt.Printf("%s: %s\n", key, value)
+		db[key] = value
+	}
+
+}
+
+func writeBoltTest(size int) {
+	db := prepBolt()
+	defer db.Close()
+
+	var key string
+	var value string
+	err := db.Update(func(tx *bolt.Tx) error {
+		for i := 0; i < size; i++ {
+			key, value = keyValue(i)
+			b := tx.Bucket(bucket)
+			err := b.Put([]byte(key), []byte(value))
+			if err != nil {
+				return err
+			}
+		}
 		return nil
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
+}
+
+func main() {
+	hellobolt()
+	size := 1000000
+	start := time.Now()
+	writeMapTest(size)
+	mapTime := time.Since(start)
+	fmt.Printf("Map Test took: %s\n", mapTime)
+	start = time.Now()
+	writeBoltTest(size)
+	boltTime := time.Since(start)
+	fmt.Printf("Bolt Test took: %s\n", boltTime)
+	fmt.Printf("bolt/map: %1.1fX\n",
+		float64(boltTime.Nanoseconds())/float64(mapTime.Nanoseconds()))
 }
